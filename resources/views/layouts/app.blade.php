e00bd79791888f0b6342986c2763539217cab93a
oji1029 <angkotasanamri@gmail.com>
2025-06-29 22:29:32 +0900

    Kesalahan Pada Cdn [fixes #3]

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -193,7 +193,7 @@
             });
         </script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script src="https://cdn.jsdelivr.net/npm/echarts@5/dist/echarts.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
         @stack('scripts')
 
